a83493d1f76b4c517cc58b68766f510847dbb569
kara87022 <karanveeraujla87@gmail.com>
2025-12-07 12:50:02 +0530

    Auto-sync: file changes

--- apps/crm-backend/src/routes/crm/tickets.js
+++ apps/crm-backend/src/routes/crm/tickets.js
@@ -5,10 +5,38 @@ const { checkPermission } = require('../../middleware/rbac');
 const { validateRequest } = require('../../middleware/validate');
 const validators = require('../../validators/tickets');
 
-router.post('/', checkPermission('create:ticket'), validators.createTicket, validateRequest, controller.createTicket);
-router.get('/', checkPermission('view:ticket'), controller.listTickets);
-router.get('/:id', checkPermission('view:ticket'), controller.getTicket);
-router.put('/:id', checkPermission('update:ticket'), validators.updateTicket, validateRequest, controller.updateTicket);
-router.delete('/:id', checkPermission('delete:ticket'), controller.deleteTicket);
+function safeHandler(fn) {
+	return async (req, res, next) => {
+		try {
+			if (typeof fn !== 'function') {
+				return res.status(500).json({ error: 'Controller method not implemented' });
+			}
+			await fn(req, res, next);
+		} catch (err) {
+			next(err);
+		}
+	};
+}
+
+router.post(
+	'/',
+	checkPermission('create:ticket'),
+	validators && validators.createTicket ? validators.createTicket : (req, res, next) => next(),
+	validateRequest,
+	safeHandler(controller.createTicket)
+);
+
+router.get('/', checkPermission('view:ticket'), safeHandler(controller.listTickets));
+router.get('/:id', checkPermission('view:ticket'), safeHandler(controller.getTicket));
+
+router.put(
+	'/:id',
+	checkPermission('update:ticket'),
+	validators && validators.updateTicket ? validators.updateTicket : (req, res, next) => next(),
+	validateRequest,
+	safeHandler(controller.updateTicket)
+);
+
+router.delete('/:id', checkPermission('delete:ticket'), safeHandler(controller.deleteTicket));
 
 module.exports = router;
